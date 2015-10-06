2c92897d35c2f860b0eba7d697ae925829be87a5
Tuan Chau <tuanchauict@gmail.com>
2015-10-06 11:18:00 +0700

    fix crazy commit

--- app/src/main/java/com/orhanobut/loggersample/MainActivity.java
+++ app/src/main/java/com/orhanobut/loggersample/MainActivity.java
@@ -3,23 +3,10 @@ package com.orhanobut.loggersample;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
-<<<<<<< HEAD
-import com.orhanobut.logger.Printer;
-
-
-public class MainActivity extends ActionBarActivity {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private static final Printer LOG = Printer.getLocalLogger();
-    private static final Printer LOG2 = Printer.getLocalLoggerWithTag("Con heo");
-    private static final Printer LOG3 = Printer.getLocalLoggerWithTagAndMethodCount("Con ca keo", 5);
-
-=======
 import com.orhanobut.logger.LLogger;
 import com.orhanobut.logger.Logger;
 
 
->>>>>>> c89c40a94c79ee7c897c7d95bb25deb47d021f61
 
 public class MainActivity extends ActionBarActivity {
     @Override
@@ -28,15 +15,10 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
 
-<<<<<<< HEAD
-        Printer LOG1 = Printer.getLocalLogger();
-        Printer LOG2 = Printer.getLocalLoggerWithTag("YOUR TAG");
-        Printer LOG3 = Printer.getLocalLoggerWithTagAndMethodCount("YOUR NEW TAG", 3);
-=======
+
         LLogger LOG1 = LLogger.getLocalLogger();
         LLogger LOG2 = LLogger.getLocalLoggerWithTag("YOUR TAG");
         LLogger LOG3 = LLogger.getLocalLoggerWithTagAndMethodCount("YOUR NEW TAG", 3);
->>>>>>> c89c40a94c79ee7c897c7d95bb25deb47d021f61
 
         LOG1.d("This with default tag");
         LOG2.d("This changes tag to YOUR TAG");
@@ -47,15 +29,20 @@ public class MainActivity extends ActionBarActivity {
         LOG1.on();
         LOG1.d("This is logged again!");
 
-        Printer.globalOff();
+        LLogger.globalOff();
         LOG1.d("NOTHING");
         LOG2.d("IS");
         LOG3.d("SHOWN");
-        Printer.globalOn();
+        LLogger.globalOn();
         LOG1.d("Yeah! I'm here");
 
         //Old fashion
         Logger.d("This is an old fashion which is static");
+
+        //Version 1.0.2
+        LLogger LOG4 = LLogger.getLocalLogger().withTag("TAG").withMethodCount(4).on();
+        LLogger LOG5 = LLogger.getLocalLogger().withTag("TAG").withMethodCount(4).off();
+
     }
 
 
